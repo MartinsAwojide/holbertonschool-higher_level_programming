59035f90c2ee586f6db1562d0a649bab1c4edc31
JUCORTESA <984@holbertonschool.com>
2019-09-10 20:08:24 +0000

    [Fix] change and condition

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -2,8 +2,9 @@
 for i in range(0, 10):
     for j in range(0, 10):
         if j > i:
-            if i == 8 and j == 9:
-                print('{}{}'.format(i, j))
-            else:
+            if i < 8:
                 print('{}{},'.format(i, j), end=' ')
+            else:
+                print('{}{}'.format(i, j))
+
 
