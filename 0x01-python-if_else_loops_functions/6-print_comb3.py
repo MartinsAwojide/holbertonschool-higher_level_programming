d131c1d7fbccec87e6a62798c3503d6570626761
JUCORTESA <984@holbertonschool.com>
2019-09-10 22:25:45 +0000

    [Fix] :d

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -3,8 +3,8 @@ for i in range(0, 10):
     for j in range(0, 10):
         if j > i:
             if i < 8:
-                print('{}{}'.format(i, j), end=', ')
+                print('{:d}{:d}'.format(i, j), end=', ')
             else:
-                print('{}{}'.format(i, j))
+                print('{:d}{:d}'.format(i, j))
 
 
