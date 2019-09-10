82403bf46e32be2fdaaf06ce401895efdeda203e
JUCORTESA <984@holbertonschool.com>
2019-09-10 20:28:40 +0000

    [Fix] ascii

--- 0x01-python-if_else_loops_functions/7-islower.py
+++ 0x01-python-if_else_loops_functions/7-islower.py
@@ -1,6 +1,6 @@
-#!/usr/bin/env python3
+#!/usr/bin/python3
 def islower(c):
-    if ord(c) >= 97 and ord(c) <= 127:
+    if (ord(c) > 96) and (ord(c) < 123):
         return True
     else:
         return False
