22cf996a6cf172726663462553c37630581163a0
JUCORTESA <984@holbertonschool.com>
2019-09-17 17:11:56 +0000

    [Fix] = len

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -5,7 +5,7 @@ def replace_in_list(my_list, idx, element):
             my_list.pop(idx)
             my_list.insert(idx, element)
             return (my_list)
-        elif idx > len(my_list):
+        elif idx >= len(my_list):
             return (my_list)
         elif idx < 0:
             return (my_list)
