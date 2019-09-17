903329b0bbe64f98e12146fa1cbaccf217098d06
JUCORTESA <984@holbertonschool.com>
2019-09-17 23:15:35 +0000

    [Fix] condition & delete loop

--- 0x03-python-data_structures/11-delete_at.py
+++ 0x03-python-data_structures/11-delete_at.py
@@ -1,10 +1,7 @@
 #!/usr/bin/python3
 def delete_at(my_list=[], idx=0):
-    for x in my_list:
-        if idx == my_list.index(x):
-            my_list.remove(x)
-            return (my_list)
-        elif idx >= len(my_list):
-            return (my_list)
-        elif idx < 0:
-            return (my_list)
+    if idx < 0 or idx >= len(my_list):
+        return (my_list)
+    else:
+        del my_list[idx]
+        return (my_list)
