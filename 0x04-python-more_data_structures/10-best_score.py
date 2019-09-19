14206e5e2b6ddb06570d787d4f8895946803cecb
JUCORTESA <984@holbertonschool.com>
2019-09-19 19:46:25 +0000

    [Fix] add condition

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -2,5 +2,7 @@
 def best_score(a_dictionary):
     if a_dictionary is None:
         return None
+    elif len(a_dictionary) is 0:
+        return None
     else:
         return (max(a_dictionary))
