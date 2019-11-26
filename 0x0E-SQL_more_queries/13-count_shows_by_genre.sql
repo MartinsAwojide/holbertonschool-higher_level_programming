3fd8676c3e872cff281108081d457b9a397fc949
JUCORTESA <984@holbertonschool.com>
2019-11-26 23:38:45 +0000

    [add] genres and shows

---
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -0,0 +1,7 @@
+-- A script that lists all genres from hbtn_0d_tvshows and displays the number of shows linked to each
+SELECT tv_genres.name AS genre, COUNT(*) AS number_shows
+FROM tv_show_genres
+JOIN tv_genres
+ON tv_show_genres.genre_id = tv_genres.id
+GROUP BY tv_show_genres.genre_id
+ORDER BY number_shows DESC;
