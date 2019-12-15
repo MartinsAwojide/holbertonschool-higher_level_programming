3ed00e394350023485d0ec93a13ea7f878af2921
jucortesa <984@holbertonschool.com>
2019-12-15 16:50:46 +0000

    [fix] spaces

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -12,8 +12,8 @@ if __name__ == "__main__":
                          db=argv[3], port=3306)
 
     cur = db.cursor()
-    cur.execute("SELECT * FROM states WHERE name LIKE 'N%'\
-		 ORDER BY states.id")
+    cur.execute("SELECT * FROM states\nWHERE name LIKE'N%'\
+                ORDER BY states.id")
     rows = cur.fetchall()
     for row in rows:
         print(row)
