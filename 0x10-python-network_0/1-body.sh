be970d81efda0ba7dca424537bf2a4983ce57d06
jucortesa <984@holbertonschool.com>
2020-01-06 21:27:04 +0000

    [fix]

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# A script that takes in a URL, sends a GET request to the URL, and displays the body of the response
+# Script that GET request to URL & shows body of response
 curl -sL "$1"
