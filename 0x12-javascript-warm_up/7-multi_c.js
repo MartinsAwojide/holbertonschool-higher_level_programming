7af1068e05dcab015f351747fe2c62f78749046f
jucortesa <juan1015@gmail.com>
2020-02-04 21:28:49 +0000

    [Add] multiple prints

---
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+const number = parseInt(process.argv[2], 10);
+if (isNaN(number)) {
+  console.log('Missing number of occurrences');
+} else {
+  for (let i = 0; i < number; i++) {
+    console.log('C is fun');
+  }
+}
