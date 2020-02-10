3778286ca6070a15c137a8ccd77fe7e443ead169
jucortesa <juan1015@gmail.com>
2020-02-10 19:51:10 +0000

    [add] occurrences in a list

---
+++ 0x13-javascript_objects_scopes_closures/7-occurrences.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+exports.nbOccurences = function (list, searchElement) {
+  let ocurr = 0;
+  for (let count = 0; count < list.length; count++) {
+    if (list[count] == searchElement) {
+      ocurr = ocurr + 1;
+    }
+  }
+  return ocurr;
+};
