83f653b009683f2a3d0d222837f10ebcf499b834
jucortesa <juan1015@gmail.com>
2020-02-10 19:52:43 +0000

    [fix] condition

--- 0x13-javascript_objects_scopes_closures/7-occurrences.js
+++ 0x13-javascript_objects_scopes_closures/7-occurrences.js
@@ -2,7 +2,7 @@
 exports.nbOccurences = function (list, searchElement) {
   let ocurr = 0;
   for (let count = 0; count < list.length; count++) {
-    if (list[count] == searchElement) {
+    if (list[count] === searchElement) {
       ocurr = ocurr + 1;
     }
   }
