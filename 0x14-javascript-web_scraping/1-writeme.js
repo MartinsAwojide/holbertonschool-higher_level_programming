50b16955e68858eddd6be9e411b687b282fe998d
jucortesa <juan1015@gmail.com>
2020-02-11 17:37:18 +0000

    [fix] delete read

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -1,11 +1,5 @@
 #!/usr/bin/node
-
 const fs = require('fs');
-
 fs.writeFile(process.argv[2], process.argv[3], 'utf-8', function (err) {
   if (err) { console.log(err); }
-});
-fs.readFile(process.argv[2], 'utf-8', (err, data) => {
-  if (err) { console.log(err); }
-  console.log(data);
 });
